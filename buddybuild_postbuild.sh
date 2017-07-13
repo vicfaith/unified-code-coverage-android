aeed8c2b927a667dec53084333515fc2b90517fe
dkang-wow <dkang@woolworths.com.au>
2017-07-13 23:44:40 +1000

    check ec file

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -3,5 +3,7 @@
 # generate code coverage
 ./gradlew clean jacocoTestReport
 
+find . -name "*coverage.ec" -print
+
 # upload jacoco report to codecov
 bash <(curl -s https://codecov.io/bash) -t "$CODECOV_TOKEN"
