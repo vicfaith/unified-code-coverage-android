b22ea03280c6dc65dac3183dd3f7a7c763c52d37
dkang-wow <dkang@woolworths.com.au>
2017-07-13 21:34:55 +1000

    upload only junit test coverage

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-  # generate code coverage
-  ./gradlew clean jacocoTestReport
+# generate code coverage
+./gradlew clean jacocoTestReport
 
-  # upload jacoco report to codecov
-  bash <(curl -s https://codecov.io/bash) 
+# upload jacoco report to codecov
+bash <(curl -s https://codecov.io/bash) -t "$CODECOV_TOKEN"
